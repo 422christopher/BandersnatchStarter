6269bc5a53144912bbb2556bc79e40e4f0848301
422christopher <422christopher@gmail.com>
2023-04-20 21:27:23 -0600

    Ticket #1. I updated the database class and created the functions to seed, reset, count, dataframe, and to_html the monster data. I also updated the main.py file in order to render 1000 random monsters every time you load that page.

--- app/data.py
+++ app/data.py
@@ -8,18 +8,38 @@ from pymongo import MongoClient
 
 
 class Database:
+    """
+    This class manages the monster database. It populates monsters,
+    resets the monster table, counts the monsters on the table,
+    and creates a dataframe of the monster table that can be
+    rendered as an HTML table on the website.
+    """
+    load_dotenv()
+    database = MongoClient(getenv("DB_URL"), tlsCAFile=where())["Database"]
 
-    def seed(self, amount):
-        pass
+    def __init__(self, collection: str):
+        self.collection = self.database[collection]
 
+    # Inserts a specified number of documents into the collection.
+    def seed(self, amount: int):
+        self.monsters = [Monster().to_dict() for i in range(amount)]
+        return self.collection.insert_many(self.monsters)
+
+    # Deletes all documents from the collection.
     def reset(self):
-        pass
+        return self.collection.delete_many({})
 
+    # Returns the number of documents in the collection.
     def count(self) -> int:
-        pass
+        return self.collection.count_documents({})
 
+    # Returns a DataFrame containing all documents in the collection
     def dataframe(self) -> DataFrame:
-        pass
+        return DataFrame(list(self.collection.find({})))
 
+    # Returns an HTML table representation of the DataFrame, or None if the collection is empty.
     def html_table(self) -> str:
-        pass
+        if self.count() > 0:
+            return self.dataframe().to_html()
+        else:
+            return "The collection is empty."
